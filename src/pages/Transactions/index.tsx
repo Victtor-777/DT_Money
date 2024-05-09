8ea82a84beddfa5b789366b076c7b21cf20da7e7
Victtor777 <victtor.eng.software@gmail.com>
2024-05-09 16:24:14 -0300

    Add Context Selector

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,3 @@
-import { useContext } from "react";
 import { Header } from "../../Components/Header";
 import { Summary } from "../../Components/Summary";
 import { SearchForm } from "./Components/SearchForm";
@@ -9,9 +8,12 @@ import {
 } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { dateFormatter, priceFormatter } from "../../utils/formatter";
+import { useContextSelector } from "use-context-selector";
 
 export function Transaction() {
-  const { transactions } = useContext(TransactionsContext);
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions;
+  });
 
   return (
     <div>
